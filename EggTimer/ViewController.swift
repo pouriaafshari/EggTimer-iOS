c3d111c70dd4bd7ab7c71d4ede876e1bbc27ee5d
pouriaafshari <pooriyaafshari81@gmail.com>
2024-09-02 17:49:52 +0200

    Added progress bar and alarm sound when Done

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,15 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-    let EggTimes: [String : Int] = ["Soft": 5, "Medium": 7, "Hard": 12]
-
+    @IBOutlet weak var dialogueLabel: UILabel!
+    @IBOutlet weak var timerBar: UIProgressView!
+    
+    let EggTimes: [String : Int] = ["Soft": 300, "Medium": 420, "Hard": 700]
+    
+    var secondsRemaining: Int? = nil
+    var timer: Timer = Timer()
+    var player: AVAudioPlayer!
+    
     @IBAction func eggPressed(_ sender: UIButton) {
-        
+                
         let hardness: String? = sender.currentTitle
-        print(EggTimes[hardness!]!)
+        let eggTime: Int! = EggTimes[hardness!]!
+        self.dialogueLabel.text = hardness!
+        secondsRemaining = EggTimes[hardness!]!
+        timerBar.progress = 0.0
+        
+        timer.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { Timer in
+            if self.secondsRemaining! > 0 {
+                var progress: Float = Float(self.secondsRemaining!)/Float(eggTime)
+                self.timerBar.progress = 1 - progress
+                self.secondsRemaining!-=1
+            } else {
+                Timer.invalidate()
+                self.dialogueLabel.text = "Done!"
+                self.playSound()
+                self.timerBar.progress = 1.0
+            }
+        }
     }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
+    }
+    
     
 }
